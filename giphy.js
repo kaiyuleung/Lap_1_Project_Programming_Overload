bbca0b1dbf170c8028ca76f18c3665dc0c2a01c7
kroles1 <korneliaoles@gmail.com>
2022-10-03 17:13:54 +0100

    demo giphy

---
+++ giphy.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const apiUrl = 'https://api.giphy.com/v1/gifs/search?api_key=XEAkZeR976diLYKcNhMlxn8S9Uvbbfza&rating=pg&q=';
+const limitVal = 20;
+const main = document.querySelector(".results");
+const form = document.querySelector(".form");
+const searchInput = document.getElementById("search");
+
+function clearPreviousResults() {
+    while (main.firstChild) {
+        main.removeChild(main.firstChild);
+    }
+}
+
+function createImages(gifs) {
+    for (const gif of gifs) {
+        const img = document.createElement("img");
+        const gifSrc = gif.images.fixed_height.url;
+
+        img.src = gifSrc;
+        img.alt = "Gif";
+        img.classList.add("results-gif");
+        
+        main.append(img);
+    }
+}
+
+async function getGifs(event) {
+    event.preventDefault();
+    clearPreviousResults();
+
+    const searchInputValue = searchInput.value;
+    const gifyUrl = `${apiUrl}${searchInputValue}&limit=${limitVal}`;
+    const response = await fetch(gifyUrl, { mode: "cors" });
+    const data = await response.json();
+    const gifData = await data.data;
+
+    createImages(gifData);
+    searchInput.value = "";
+}
+
+form.addEventListener("submit", getGifs);
